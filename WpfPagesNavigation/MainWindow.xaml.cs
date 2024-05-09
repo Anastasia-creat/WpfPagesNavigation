03811dc7a6ac1d67eaba12906369d613a34c512a
Anastasia <kochkina0226@mail.ru>
2024-05-09 22:32:30 +0500

    Добавьте файлы проекта.

---
+++ WpfPagesNavigation/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPagesNavigation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+
+
+            string n = btn.Tag.ToString();
+            Uri uri = new Uri($"Pages/Page{n}.xaml", UriKind.Relative);
+
+            mainFrame.Navigate(uri);
+        }
+    }
+}
